0bbc3dd345494b9d0cc54de88840dd600c6d729e
muxigame <76636667+muxigame@users.noreply.github.com>
2023-09-05 20:38:34 +0800

    updata window server

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +34,26 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        brightness: Brightness.dark,
+        colorScheme: const ColorScheme(
+            brightness: Brightness.dark,
+            inversePrimary:Color.fromARGB(255, 18, 18, 18),
+            primary:Color(0xffbb86fc),
+            onPrimary : Colors.black,
+            secondary : Color(0xff03dac6),
+            onSecondary : Colors.black,
+            error : Color(0xffcf6679),
+            onError : Colors.black,
+            background : Color(0xff121212),
+            onBackground : Colors.white,
+            surface : Color(0xff121212),
+            onSurface:  Colors.white,
+            secondaryContainer:Color.fromARGB(255, 47, 47, 47),
+        ),
+        textTheme: TextTheme(),
+        primaryColor: Colors.black87,
+        shadowColor: Colors.black12,
+        listTileTheme: ListTileThemeData(),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -71,12 +91,10 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
 
-
       connect();
       _counter++;
     });
   }
-
 
   Future<void> connect() async {
     String batteryLevel;
@@ -92,7 +110,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> setApplicationPath() async {
     String batteryLevel;
     try {
-      final int result = await  platform.invokeMethod("ApplicationPath", Platform.resolvedExecutable);
+      final int result = await platform.invokeMethod(
+          "ApplicationPath", Platform.resolvedExecutable);
     } on PlatformException catch (e) {
       log("Failed to get battery level: '${e.message}'.");
     }
@@ -107,43 +126,81 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
+      // appBar: AppBar(
+      //   // TRY THIS: Try changing the color here to a specific color (to
+      //   // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+      //   // change color while the other colors stay the same.
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      // ),
+      body: ListView(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        children: [
+          Container(
+            width: double.infinity,
+            height: 60,
+            margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondaryContainer,
+              borderRadius: BorderRadius.circular(8),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  'images/flag/cn.svg',
+                  width: 50,
+                  height: 30,
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(bottom: 3,left: 10),
+                    child: const Text(
+                      '中国-香港',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 3),
+                  child: const Text(
+                    "200ms",
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+          Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
